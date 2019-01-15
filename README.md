3ce2f90ca20193294b21f5b9bbbdc7485492e6ff
Dino Nguyen and Matt Groberg <dino@>
2019-01-14 16:27:28 -0800

    add cipher functionality with js and jQuery

---
+++ README.md
@@ -0,0 +1,32 @@
+# Sentence Cipher
+
+#### _version 1 01/14/2019_
+
+#### By _**Dino Nguyen and Matt Groberg**_
+
+## Description
+
+_This was a homework assignment for Epicodus. We practiced modifying strings and writing functions. Enter a sentence in the prompt, then to see your sentence, click on the good dog. To see the jumbled sentence, click the bad dog (be careful, he bites!)._
+
+## Setup/Installation Requirements
+
+* _git clone this repository to test it out yourself_
+
+_{Leave nothing to chance! You want it to be easy for potential users, employers and collaborators to run your app. Do I need to run a server? How should I set up my databases? Is there other code this app depends on?}_
+
+## Known Bugs
+
+_No known bugs_
+
+## Support and contact details
+
+_email us at grobergmg@gmail.com_
+_or dinodnguyen@gmail.com_
+
+## Technologies Used
+
+_HTML, CSS, bootstrap, javaScript, jQuery!_
+
+### License
+
+Copyright (c) 2016 **_Matt Groberg and Dino Nguyen_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,48 @@
+var sentence = prompt("Enter a sentence, user...");
+// console.log(sentence);
 
-alert("test");
+
+function capitalizer(){
+  var capitalizedLetters = sentence[0].toUpperCase() + sentence[sentence.length-1].toUpperCase();
+  return capitalizedLetters;
+}
+
+// var firstAndLastCapitalized = capitalizer();
+
+function reverseCapitalizer(letters) {
+  return letters[1]+letters[0]
+}
+
+// alert("it Worked "+ reverseCapitalizer(capitalizer()))
+
+function allInOne(){
+var newSentence= sentence + reverseCapitalizer(capitalizer());
+return newSentence;
+}
+
+// console.log(allInOne());
+
+function sentenceWithNewFirstLetter() {
+  var middle = Math.floor((sentence.length)/2);
+  var middleLetter = sentence[middle];
+  return middleLetter + allInOne();
+}
+
+// console.log(sentenceWithNewFirstLetter());
+
+function reversedCode(){
+  var reversedSentence=sentenceWithNewFirstLetter();
+  var sentenceArray=reversedSentence.split("");
+  var reversedArray=sentenceArray.reverse();
+  var joinedArray=reversedArray.join("");
+  return joinedArray;
+}
+
+$(document).ready(function() {
+  $("#goodDog").click(function() {
+    alert(sentence);
+  });
+  $("#badDog").click(function() {
+    alert(reversedCode());
+  })
+});
